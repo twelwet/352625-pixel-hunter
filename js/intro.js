5ad3fb8b0a9a280ae5489f0c899ec57d5d9ab127
Maxim Akimov <akimovmax@mail.ru>
2018-08-19 19:25:44 +0300

    Не работает импорт между модулями

---
+++ js/intro.js
@@ -0,0 +1,14 @@
+// intro.js
+
+import render from './js/util.js';
+
+const template = `
+  <section class="intro">
+    <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+  </section>
+`;
+
+const element = render(template);
+
+export default element;
